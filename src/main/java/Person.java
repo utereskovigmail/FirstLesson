10def3fe5068f82243f818d5806b579b29808d3b
George <utereskovygmail.com@MacBook-Pro.local>
2026-04-18 17:35:07 +0300

    Person class with constructor

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -5,4 +5,25 @@ import lombok.Data;
 public class Person {
     String name;
     int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(int age) {
+        this.age = age;
+    }
+
+    public void SayHello() {
+        System.out.println("Hello, my name is " + name);
+    }
+
+    public void Me(){
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
 }
